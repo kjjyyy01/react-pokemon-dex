428f29714350f82d7a632c5524153cbc9d964489
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-06 10:22:23 +0900

    Feature: 포켓몬 리스트및 선택기능 구현

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,40 +1,39 @@
-const data = [
-  {
-    img_path: "/image/monsterBall-img.png",
-  },
-  {
-    img_path: "/image/monsterBall-img.png",
-  },
-  {
-    img_path: "/image/monsterBall-img.png",
-  },
-  {
-    img_path: "/image/monsterBall-img.png",
-  },
-  {
-    img_path: "/image/monsterBall-img.png",
-  },
-  {
-    img_path: "/image/monsterBall-img.png",
-  },
-];
+import styled from "styled-components";
+import PokemonCard from "./PokemonCard";
 
-const Dashboard = () => {
+const StTitle = styled.h1`
+  font-size: 2rem;
+  color: #cd3939;
+`;
+
+const Dashboard = ({ selectedPokemon, setSelectedPokemon }) => {
+  const pokemonBallCount = 6 - selectedPokemon.length;
+
+  const cancelPokemon = (id) => {
+    setSelectedPokemon(selectedPokemon.filter((pokemon) => pokemon.id !== id));
+  };
+
   return (
-    <>
-      <header>
-        <h1>나만의 포켓몬 도감</h1>
-        <ul>
-          {data.map((element) => {
-            return (
-              <li key={element.img_path}>
-                <img src="/image/monsterBall-img.png" alt="" />
+    <header>
+      <StTitle>나만의 포켓몬 도감</StTitle>
+      <ul>
+        {selectedPokemon.map((pokemon) => (
+          <li key={pokemon.id}>
+            <PokemonCard pokemon={pokemon} />
+            <button onClick={() => cancelPokemon(pokemon.id)}>취소</button>
+          </li>
+        ))}
+
+        {pokemonBallCount > 0 &&
+          Array(pokemonBallCount)
+            .fill(null)
+            .map((_, index) => (
+              <li key={`${index}`}>
+                <img src="/image/monsterBall-img.png" alt="빈 포켓볼" />
               </li>
-            );
-          })}
-        </ul>
-      </header>
-    </>
+            ))}
+      </ul>
+    </header>
   );
 };
 
