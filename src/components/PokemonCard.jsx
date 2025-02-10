502409f7ff9716f15ae55ce069b5bedfc5012ac0
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-10 17:14:16 +0900

    #38 fix: 코드 수정

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,25 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addPokemon } from "../redux/slices/pokemonSlices";
 import { StCard, StCardName, StCardNumber, StImg, StLink, StListButton } from "./Style";
+import { toast } from "react-toastify";
 
 const PokemonCard = ({ pokemon }) => {
   const dispatch = useDispatch();
+  const pokemonList = useSelector((state) => state.pokemon);
+
+  const addPokemonHandler = () => {
+    if (pokemonList.length >= 6) {
+      toast.warn("최대 6마리까지 추가할 수 있습니다.");
+      return;
+    }
+
+    if (pokemonList.find((poke) => poke.id === pokemon.id)) {
+      toast.warn("이미 추가된 포켓몬입니다!");
+      return;
+    }
+
+    dispatch(addPokemon(pokemon));
+  };
 
   return (
     //* MOCK_DATA에서 하나씩 순회한 포켓몬 id를 pokemon으로 설정
@@ -13,7 +29,7 @@ const PokemonCard = ({ pokemon }) => {
         <StCardName>{pokemon.korean_name}</StCardName>
         <StCardNumber>No.{pokemon.id.toString().padStart(3, "0")}</StCardNumber>
       </StLink>
-      <StListButton onClick={() => dispatch(addPokemon(pokemon))}>추가</StListButton>
+      <StListButton onClick={addPokemonHandler}>추가</StListButton>
     </StCard>
   );
 };
