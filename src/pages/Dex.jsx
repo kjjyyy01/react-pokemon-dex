2e5fcc2ffcefe39b88ca1b9acbb59346b78b49f2
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-10 20:34:27 +0900

    feature: Detail에서 추가 기능

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,14 +1,12 @@
-import { Provider } from "react-redux";
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
-import store from "../redux/config/store";
 
 const Dex = () => {
   return (
-    <Provider store={store}>
+    <>
       <Dashboard />
       <PokemonList />
-    </Provider>
+    </>
   );
 };
 
